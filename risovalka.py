d7a6981f7b9c68a9c233ec081e0b84157c6356f0
vic1267 <urfin1@tut.by>
2022-01-06 12:09:48 +0300

    there are changes

--- risovalka.py
+++ risovalka.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
+from random import random
+from kivy.core.window import Window
 from kivy.graphics import (Color, Ellipse, Rectangle, Line)
 
 
@@ -8,7 +10,7 @@ class PainterWidget(Widget):
 
     def on_touch_down(self, touch):
         with self.canvas:
-            Color(0, 1, 0, 1)
+            Color(random(), random(), random(), 1)
             rad = 20
             Ellipse(pos=(touch.x - rad / 2, touch.y - rad / 2), size=(rad, rad))
             touch.ud['Line'] = Line(points=(touch.x, touch.y), width=10)
@@ -22,11 +24,21 @@ class PainApp(App):
         parent = Widget()
         self.painter = PainterWidget()
         parent.add_widget(self.painter)
-        parent.add_widget(Button(text='Clear', on_press = self.clear_canvas, size=(100, 50)))
+        parent.add_widget(Button(text='Clear', on_press=self.clear_canvas, size=(250, 150)))
+        parent.add_widget(Button(text='Save', on_press=self.save, size=(250, 150), pos=(250, 0)))
+        parent.add_widget(Button(text='Screen', on_press=self.screen, size=(250, 150), pos=(500, 0)))
         return parent
 
     def clear_canvas(self, instance):
         self.painter.canvas.clear()
+
+    def save(self, instance):
+        self.painter.size = (Window.size[0], Window.size[1])
+        self.painter.export_to_png('image.png')
+
+
+    def screen(self, instance):
+        Window.screenshot('screen.png')
 
 
 if __name__ == '__main__':
